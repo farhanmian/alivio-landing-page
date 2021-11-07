c72766e5c26a3022e9346833c87446f031adcd1d
farhanmian <farhanmian099@gmail.com>
2021-11-07 15:00:15 +0530

    change some code

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Base from '../src/components/Base/Base'
 import Html from '../src/common/theme/Html';
+import { Fragment } from 'react';
 
 export default function Home() {
   return (
-    <main>
+    <Fragment>
       <Html />
       <Base />
-    </main>
+    </Fragment>
   )
 }
--- src/components/Base/Base.js
+++ src/components/Base/Base.js
@@ -2,18 +2,15 @@ import { ThemeProvider } from "@material-ui/styles";
 import { theme } from "../../common/theme/theme";
 
 import Header from "../HomePage/Header/Header";
-import Nav from "../../common/Nav/Nav";
 import Steps from "../HomePage/Steps/Steps";
 import Divider from "../../common/Divider/Divider";
 import Review from "../HomePage/Review/Review";
 import Product from "../HomePage/Product/Product";
-import Footer from "../../common/Footer/Footer";
 
 
 const Base = () => {
     return (
         <ThemeProvider theme={theme}>
-            <Nav />
             <Header />
             <Divider />
 
@@ -24,9 +21,6 @@ const Base = () => {
             <Divider />
 
             <Product />
-            <Divider />
-
-            <Footer />
         </ThemeProvider >
     )
 }
